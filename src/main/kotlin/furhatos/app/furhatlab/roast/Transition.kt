2876fd9ff525c0e8f75091d0c2464db854c5b8ae
marcuspahlman <mpahlmans@gmail.com>
2025-12-02 11:55:54 +0100

    Update transition lines and add delay

--- src/main/kotlin/furhatos/app/furhatlab/roast/Transition.kt
+++ src/main/kotlin/furhatos/app/furhatlab/roast/Transition.kt
@@ -5,11 +5,14 @@ import furhatos.app.furhatlab.flow.chat.ChatState
 import furhatos.flow.kotlin.State
 import furhatos.flow.kotlin.furhat
 import furhatos.flow.kotlin.state
+import furhatos.gestures.Gestures
 
 val Transition: State = state {
     onEntry {
         furhat.say(" Where do you see yourself in 10 years?")
-        furhat.say("Okay, I got enough. Let's see what we have")
+        delay(5000)
+        furhat.gesture(Gestures.GazeAway)
+        furhat.say("That's literally soo interesting...")
         goto(Roast)
     }
 
