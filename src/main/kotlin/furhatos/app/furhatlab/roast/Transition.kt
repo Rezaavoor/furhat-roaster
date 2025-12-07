9d90eca1392cd6f83ea44669e11364d76362bce1
marcuspahlman <mpahlmans@gmail.com>
2025-12-07 17:17:55 +0100

    Refactor Transition state to improve transition to roasting

--- src/main/kotlin/furhatos/app/furhatlab/roast/Transition.kt
+++ src/main/kotlin/furhatos/app/furhatlab/roast/Transition.kt
@@ -7,10 +7,14 @@ import furhatos.gestures.Gestures
 
 val Transition: State = state {
     onEntry {
-        furhat.say(" Where do you see yourself in 10 years?")
-        delay(5000)
+        furhat.say("Okay, I like playing Fortnight and hanging out with my Alpha squad.")
+        delay(400)
+        furhat.say("Where do you see yourself in 10 years?")
+        delay(4000)
         furhat.gesture(Gestures.GazeAway)
         furhat.say("Wow, that's literally so interesting...")
+        delay(400)
+        furhat.say("Now, let's begin the roast battle. I'll go first.")
         goto(Roast)
     }
 
